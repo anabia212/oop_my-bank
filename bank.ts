e0f96a3f5573fc754d511013adb3516c0388e54c
Anabia Anabia <164624108+anabia212@users.noreply.github.com>
2024-07-12 21:19:16 +0500

    Add files via upload

---
+++ bank.ts
@@ -0,0 +1,67 @@
+interface ibankaccount{
+    credit(amount:number): void
+    debit(amount:number): void
+}
+class Bankaccount implements ibankaccount{
+    accountbalance:number;
+    constructor (accountbalance:number){
+        this.accountbalance=accountbalance
+    }
+    public credit(amount: number){
+        if(amount>0){
+            this.accountbalance+=amount
+            console.log("credit finallygot into your new account and you balance is:" +this.accountbalance);
+        }
+        else{
+            console.log("credit unsuccessful!")
+        }
+    }
+    public debit(amount:number){
+        if(amount>0 && amount<this.accountbalance){
+            this.accountbalance-=amount
+            console.log("debit successfuly shared in bank account:" +this.accountbalance)
+        }
+        else{
+            console.log("debit unsuccessful!")
+        }
+    }
+}
+
+class Customer{
+    person:{
+        firstname: string;
+        lastname: string;
+    }
+    age: number;
+    gender: string;
+    mobile_number: number;
+    bankaccount:Bankaccount;
+
+    constructor(person: {firstname:string;lastname:string;},age:number,gender:string,mobile_number:number,bankaccount:Bankaccount){
+        this.person=person;
+        this.age=age;
+        this.gender=gender;
+        this.mobile_number=mobile_number;
+        this.bankaccount=bankaccount;
+    }
+    public display(){
+        console.log("Name: "+this.person.firstname+""+this.person.lastname);
+        console.log("Age: "+this.age);
+        console.log("Gender: "+this.gender);
+        console.log("Mobile number: "+this.mobile_number);
+        console.log("Amount in Bank: "+this.bankaccount.accountbalance);
+        console.log()
+    }   
+}
+
+const a1=new Bankaccount(800)
+const c1=new Customer( {firstname:"anabia",lastname:"asma"},15,"female",32872735280284,a1);
+c1.display()
+c1.bankaccount.debit(500)
+console.log()
+
+const a2=new Bankaccount(500)
+const c2=new Customer( {firstname:"ana",lastname:"bia"},20,"male",33756302874032,a2);
+c2.display()
+c2.bankaccount.debit(200)
+console.log()
